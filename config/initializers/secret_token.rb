c80985e1da597320366f7d4a94e27e18b5bf0b04
William Hu (Macbook pro) <hu.will38@gmail.com>
2012-12-20 12:43:12 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Menulet::Application.config.secret_token = 'fc43d2ad72ab04f8b5f343529476e4cbe9342ee3c4dd1202a2bedafa1f3c1807b0b378044aeed058dc36f1184bc92a7ba60d1538bdb91ec7ce52068e3c8c3836'
